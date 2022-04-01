627c1ee8144a09d2cbdbbb7ac45797ca93b74e7e
Rioa <rioaformyking@gmail.com>
2022-04-01 15:32:18 +0800

    project-init

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+require('dotenv').config()
+
+const { json } = require('body-parser');
 const express = require('express')
 const app = express()
 
-const mongoss= require('mongodb')
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
 
+// WebSocket
+// const WebSocketServer = require('ws').Server,
+//     wss = new WebSocketServer({ port: 3303})
+// wss.on('connection', function (ws) {
+//     console.log('Sever connected!');
+//     ws.on('message', function (message) {
+//         wss.clients.forEach(function each(client) {
+//             client.send(message);
+//         });
+//         console.log(message.toString(), "msg_length:" + message.length);
+//     });
+// })
 
-app.addListener(3000, () => {
-    console.log("connected to server!");
+app.use(express.json())
+
+app.listen(3303, () => {
+    console.log("Express Server connected!");
 })
