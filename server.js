6748ac230e2b2d81da4802a5feb5c8460fed019d
Rioa <rioaformyking@gmail.com>
2022-04-13 14:53:17 +0800

    fix:express-ws

--- server.js
+++ server.js
@@ -16,20 +16,19 @@ db.on('open', () => console.log('connected to mongoDB'))
 
 //WebSocket
 enableWs(app)
-app.ws('/ws/screenshots', (ws, req) => {
+app.ws('/ws/screenshots', (ws) => {
     ws.on('message', (msg) => {
         console.log(msg)
         ws.send(msg)
     })
-})
+}) 
 
 app.ws('/ws/cars', (ws) => {
     let cars = []
     var carJson
-    ws.on('message', (msg) => {
-        ws.clients.forEach(function each(client) {
-            client.send(`${msg}`)
-        })
+    ws.on('message', (message) => {
+        //send message to all clients
+        ws.send(message)
         console.log('Received: %s', message);
         //save every 10 messages into monogoDB
         carJson = JSON.parse(message)
@@ -51,39 +50,39 @@ app.ws('/ws/cars', (ws) => {
     })
 })
 
-const WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({ 
-        port: 3304
-    })
-    let cars = []
-    var carJson
-    wss.on('connection', function (ws) {
-        console.log('WS Sever connected!');
-        ws.on('message', function (message) {
-            wss.clients.forEach(function each(client) {
-                client.send(`${message}`);
-            });
-            console.log('received: %s', message);
-            //save every 10 messages into monogoDB
-            carJson = JSON.parse(message)
-            cars.push(carJson)
+// const WebSocketServer = require('ws').Server,
+//     wss = new WebSocketServer({ 
+//         port: 3304
+//     })
+//     let cars = []
+//     var carJson
+//     wss.on('connection', function (ws) {
+//         console.log('WS Sever connected!');
+//         ws.on('message', function (message) {
+//             wss.clients.forEach(function each(client) {
+//                 client.send(`${message}`);
+//             });
+//             console.log('received: %s', message);
+//             //save every 10 messages into monogoDB
+//             carJson = JSON.parse(message)
+//             cars.push(carJson)
 
-            console.log(cars.length);
-            //if cars.length > 10, save cars to monogoDB
-            if (cars.length == 10) {
-                //print cars to console
-                // console.log(cars)
-                Car.insertMany(cars, (err) => {
-                    if (err) {
-                        console.log(err)
-                    } else {
-                        console.log('inserted')
-                    }
-                })
-                cars = []
-            }
-        });
-    })
+//             console.log(cars.length);
+//             //if cars.length > 10, save cars to monogoDB
+//             if (cars.length == 10) {
+//                 //print cars to console
+//                 // console.log(cars)
+//                 Car.insertMany(cars, (err) => {
+//                     if (err) {
+//                         console.log(err)
+//                     } else {
+//                         console.log('inserted')
+//                     }
+//                 })
+//                 cars = []
+//             }
+//         });
+//     })
 
 // routes
 const carRouter = require('./routers/car_router')
