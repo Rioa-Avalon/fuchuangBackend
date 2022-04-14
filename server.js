39ed1e14b43f7f4b426626d65223f0f33284aa2f
Rioa <rioaformyking@gmail.com>
2022-04-14 10:13:28 +0800

    add:docker

--- server.js
+++ server.js
@@ -56,40 +56,6 @@ app.ws('/ws', (ws) => {
     })
 })
 
-// const WebSocketServer = require('ws').Server,
-//     wss = new WebSocketServer({ 
-//         port: 3304
-//     })
-//     let cars = []
-//     var carJson
-//     wss.on('connection', function (ws) {
-//         console.log('WS Sever connected!');
-//         ws.on('message', function (message) {
-//             wss.clients.forEach(function each(client) {
-//                 client.send(`${message}`);
-//             });
-//             console.log('received: %s', message);
-//             //save every 10 messages into monogoDB
-//             carJson = JSON.parse(message)
-//             cars.push(carJson)
-
-//             console.log(cars.length);
-//             //if cars.length > 10, save cars to monogoDB
-//             if (cars.length == 10) {
-//                 //print cars to console
-//                 // console.log(cars)
-//                 Car.insertMany(cars, (err) => {
-//                     if (err) {
-//                         console.log(err)
-//                     } else {
-//                         console.log('inserted')
-//                     }
-//                 })
-//                 cars = []
-//             }
-//         });
-//     })
-
 // routes
 const carRouter = require('./routers/car_router')
 const expressWs = require('express-ws')
